145ee0452b6f64931dad0db7e3de7911c4054cfc
Jesús Iván Hernández Ramírez <Jesus_Hdez.Ramirez@outlook.com>
2020-07-06 18:10:34 -0500

    Primer commit del proyecto

---
+++ MemoriaRAM.Libre/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+////////////////////////////////////////////////////////
+// Descripción del programa: muestra la RAM libre     //
+// Autor del programa: Jesús Hdz. Ramírez (Virus)     //
+////////////////////////////////////////////////////////
+
+namespace MemoriaRAM.Libre
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(ramCounter.NextValue() + "MB");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
